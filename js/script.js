75fa74fcdeb52e752de1f72d18e547f244d2b63c
marwah20alshakhli <72710465+marwah20alshakhli@users.noreply.github.com>
2020-12-10 00:53:05 +0100

    updated time and date

--- js/script.js
+++ js/script.js
@@ -6,10 +6,44 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
     let windElement = document.querySelector("#wind")
-    windElement.innerHTML = `Wind: ${response.data.wind.speed}km/h`
+    windElement.innerHTML = `Wind: ${response.data.wind.speed}km/h`;
+    dateElemet = document.querySelector("#current-date");
+    dateElemet.innerHTML = formatDate(response.data.dt * 1000)
 }
 
 let apiKey = "125ae54afb30daf43aec3cdb943d26b0";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+function formatDate() {
+    let currentDate = new Date();
+    let hours = currentDate.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = currentDate.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ];
+    let date = currentDate.getDate();
+    let day = days[currentDate.getDay()];
+    let month = months[currentDate.getMonth()];
+    let formattedDate = `${day} ${month} ${date}, ${hours}:${minutes}`;
+    return formattedDate;
+}

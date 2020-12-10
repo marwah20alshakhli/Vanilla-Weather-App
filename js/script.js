49067bd20f2f77593016bd705ed3158a1d684302
marwah20alshakhli <72710465+marwah20alshakhli@users.noreply.github.com>
2020-12-10 01:02:22 +0100

    changed city parameter

--- js/script.js
+++ js/script.js
@@ -7,12 +7,13 @@ function displayTemperature(response) {
     humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
     let windElement = document.querySelector("#wind")
     windElement.innerHTML = `Wind: ${response.data.wind.speed}km/h`;
-    dateElemet = document.querySelector("#current-date");
+    let dateElemet = document.querySelector("#current-date");
     dateElemet.innerHTML = formatDate(response.data.dt * 1000)
 }
 
 let apiKey = "125ae54afb30daf43aec3cdb943d26b0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
 

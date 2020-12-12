80c1c6228b8fbe788ce705a4b465a0f9942229b8
marwah20alshakhli <72710465+marwah20alshakhli@users.noreply.github.com>
2020-12-12 19:04:26 +0100

    added some styling to links, icon attribute and fixed some HTML text

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ function displayTemperature(response) {
     dateElemet.innerHTML = formatDate(response.data.dt * 1000);
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].icon);
     celsiusTemp = response.data.main.temp;
 }
 

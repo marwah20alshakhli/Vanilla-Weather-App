fecc616a265c7a034c5be226adf11281806cf96d
marwah20alshakhli <72710465+marwah20alshakhli@users.noreply.github.com>
2020-12-13 21:47:52 +0100

    fixed the current location button

--- js/script.js
+++ js/script.js
@@ -118,7 +118,7 @@ function searchLocation(position) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
-    axios.get(apiUrl)
+    axios.get(apiUrl).then(displayTemperature);
 }
 
 let currentLocationButton = document.querySelector("#current-location");

e8684fa2e40af4da6a7fa7db857a381ca297702a
marwah20alshakhli <72710465+marwah20alshakhli@users.noreply.github.com>
2020-12-13 21:30:50 +0100

    current location js

--- js/script.js
+++ js/script.js
@@ -93,7 +93,6 @@ function formatDate() {
 function searchCity(event) {
     event.preventDefault();
     let cityInput = document.querySelector("#city-input");
-    console.log(cityInput.value);
     info(cityInput.value)
 }
 
@@ -109,6 +108,21 @@ function convertToCelsius(event) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(celsiusTemp);
 }
+
+function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function searchLocation(position) {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl)
+}
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
 let form = document.querySelector("#form-search");
 form.addEventListener("submit", searchCity);
